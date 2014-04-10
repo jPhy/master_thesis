bb6070f4f9aa40b45a12732e93856e6fa612d935
Stephan Jahn <gu92biv@mytum.de>
2014-04-10 14:56:31 +0200

    set up framework for: testing new algorithms to generate first proposal

---
+++ algorithms/gaussian_shells/params.py
@@ -0,0 +1,57 @@
+import numpy as np
+
+params = dict(
+random_seed = np.random.randint(2**62),
+dim = 2,
+kill_in_hc = True,
+k = 10, # number of markov chains
+N_burn_in = 10**3, # number of burn-in samples
+N_MCMC = 10**4, # number of steps per chain
+N_adapt = 500, # adapt the Markov chains every N_adapt steps (not during burn-in)
+K_g = 15, # components per group
+L = 100,
+critical_r = 2.,
+)
+
+# ----------------------------------------
+# do not change anything below this line !
+# ----------------------------------------
+
+default2d = dict(
+random_seed = np.random.randint(2**62),
+dim = 2,
+kill_in_hc = True,
+k = 10, # number of markov chains
+N_burn_in = 10**3, # number of burn-in samples
+N_MCMC = 10**4, # number of steps per chain
+N_adapt = 500, # adapt the Markov chains every N_adapt steps (not during burn-in)
+K_g = 15, # components per group
+L = 100,
+critical_r = 2.,
+)
+
+default10d = dict(
+random_seed = np.random.randint(2**62),
+dim = 10,
+kill_in_hc = True,
+k = 10, # number of markov chains
+N_burn_in = 10**3, # number of burn-in samples
+N_MCMC = 2.* 10**4, # number of steps per chain
+N_adapt = 500, # adapt the Markov chains every N_adapt steps (not during burn-in)
+K_g = 15, # components per group
+L = 100,
+critical_r = 2.,
+)
+
+default20d = dict(
+random_seed = np.random.randint(2**62),
+dim = 20,
+kill_in_hc = True,
+k = 10, # number of markov chains
+N_burn_in = 10**3, # number of burn-in samples
+N_MCMC = 2.* 10**4, # number of steps per chain
+N_adapt = 500, # adapt the Markov chains every N_adapt steps (not during burn-in)
+K_g = 25, # components per group
+L = 200,
+critical_r = 2.,
+)
---
+++ algorithms/mcmc.py
@@ -0,0 +1,57 @@
+from __future__ import division
+import numpy as np
+import pypmc
+
+def create_mcmc_samples(log_target, k, N_burn_in, N_MCMC, N_adapt, scale=.1):
+    """Run ``k`` markov-chains for ``N_burn_in`` steps, discard
+    and run again for ``N_MCMC`` steps whereby the proposals are
+    to be adapted every ``N_adapt`` steps.
+    Proposal covariances are initialized as np.eye(dim)*scale
+
+    """
+    dim = log_target.dim
+
+    # define the initial proposal for the local random walk markov chain
+    prop_sigma = scale * np.eye(dim)
+    prop = pypmc.density.gauss.LocalGauss(prop_sigma)
+
+    # choose random initializations to run k chains
+    starts = []
+    for i in range(k):
+        point = np.random.rand(dim)*6. - 3.
+        while np.isinf(log_target(point)):
+            point = np.random.rand(dim)*6. - 3.
+        starts.append(point)
+
+    # define the markov chain objects
+    mcs = [pypmc.sampler.markov_chain.AdaptiveMarkovChain(log_target, prop, starts[i]) for i in range(k)]
+
+
+    # run and discard burn-in
+    for i, mc in enumerate(mcs):
+        print "burn in chain", i
+        mc.run(N_burn_in)
+        mc.history.clear()
+
+
+    # run N_MCMC steps adapting the proposal every N_adapt steps
+    # hereby save the accept count which is returned by mc.run
+    accept_counts = np.zeros(k)
+    for n, mc in enumerate(mcs):
+        print "running chain", n
+        for i in range(N_MCMC//N_adapt):
+            accept_counts[n] += mc.run(N_adapt)
+            mc.adapt()
+
+    # extract a reference to the history of all visited points
+    values = [mcs[i].history[:] for i in range(k)]
+    accept_rates = np.array([float(accept_counts[i]) / len(values[i]) for i in range(k)])
+    for i, count in enumerate(accept_counts):
+        print "Chain %i accepted %4.2f%% of the proposed points" % (i, accept_rates[i] * 100)
+
+    # check optimal range for accep_rate
+    assert ( accept_rates > .15 ).all()
+    assert ( accept_rates < .35 ).all()
+
+    # return the samples
+    return values
