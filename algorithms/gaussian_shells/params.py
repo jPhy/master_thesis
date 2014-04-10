bb1cf89e0bbaed415fbe92694e4e319a2175aa52
Stephan Jahn <gu92biv@mytum.de>
2014-04-10 15:50:14 +0200

    [algorithms] add variational bayes

--- algorithms/gaussian_shells/params.py
+++ algorithms/gaussian_shells/params.py
@@ -3,21 +3,45 @@ import numpy as np
 params = dict(
 random_seed = np.random.randint(2**62),
 dim = 2,
-kill_in_hc = True,
 k = 10, # number of markov chains
 N_burn_in = 10**3, # number of burn-in samples
 N_MCMC = 10**4, # number of steps per chain
 N_adapt = 500, # adapt the Markov chains every N_adapt steps (not during burn-in)
-K_g = 15, # components per group
-L = 100,
-critical_r = 2.,
+
+N_thin = 100, # use only every `N_thin`-th sample for the variational bayes (1 means all)
+
+# how variational bayes is initialized, known keywords: ['package_default'] (see actual program for details)
+vb_initialization = 'package_default',
+N_max_vb = 100, # maximum number of steps in variational bayes
+vb_initial_K = 100,
+vb_rel_tol = 10**-10,
+vb_abs_tol = 10**-5,
 )
+
 
 # ----------------------------------------
 # do not change anything below this line !
 # ----------------------------------------
 
-default2d = dict(
+pure_variational_default = dict(
+random_seed = np.random.randint(2**62),
+dim = 2,
+k = 10, # number of markov chains
+N_burn_in = 10**3, # number of burn-in samples
+N_MCMC = 10**4, # number of steps per chain
+N_adapt = 500, # adapt the Markov chains every N_adapt steps (not during burn-in)
+
+N_thin = 100, # use only every `N_thin`-th sample for the variational bayes (1 means all)
+
+# how variational bayes is initialized, known keywords: ['package_default'] (see actual program for details)
+vb_initialization = 'package_default',
+N_max_vb = 100, # maximum number of steps in variational bayes
+vb_initial_K = 100,
+vb_rel_tol = 10**-10,
+vb_abs_tol = 10**-5,
+)
+
+original_default2d = dict(
 random_seed = np.random.randint(2**62),
 dim = 2,
 kill_in_hc = True,
@@ -30,7 +54,7 @@ L = 100,
 critical_r = 2.,
 )
 
-default10d = dict(
+original_default10d = dict(
 random_seed = np.random.randint(2**62),
 dim = 10,
 kill_in_hc = True,
@@ -43,7 +67,7 @@ L = 100,
 critical_r = 2.,
 )
 
-default20d = dict(
+original_default20d = dict(
 random_seed = np.random.randint(2**62),
 dim = 20,
 kill_in_hc = True,
