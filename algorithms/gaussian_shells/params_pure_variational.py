9c02bb9fa7d0edad99d056a1379d2d9b884ac00c
Stephan Jahn <gu92biv@mytum.de>
2014-04-16 12:01:16 +0200

    [algorithms] collect more data

--- algorithms/gaussian_shells/params_pure_variational.py
+++ algorithms/gaussian_shells/params_pure_variational.py
@@ -8,18 +8,18 @@ N_burn_in = 10**3, # number of burn-in samples
 N_MCMC = 2 * 10**4, # number of steps per chain
 N_adapt = 500, # adapt the Markov chains every N_adapt steps (not during burn-in)
 
-N_thin = 100, # use only every `N_thin`-th sample for the variational bayes (1 means all)
+N_thin = 10, # use only every `N_thin`-th sample for the variational bayes (1 means all)
 
 # how variational bayes is initialized, known keywords: ['package_default', 'initial_guess_large_nu', 'means_only'] (see actual program for details)
 vb_initialization = 'initial_guess_large_nu',
-L = 100, # patch length; only needed for vb_initialization 'initial_guess_large_nu' and 'means_only'
+L = 500, # patch length; only needed for vb_initialization 'initial_guess_large_nu' and 'means_only'
 N_max_vb = 1000, # maximum number of steps in variational bayes
 #vb_initial_K = 2,
-#vb_prune = 2, # prune components with N_k less than ``vb_prune``
 vb_rel_tol = 10**-10,
 vb_abs_tol = 10**-5,
 abandon_weights = False,
 )
+params.update((('vb_prune', params['L']/params['N_thin']),)) # prune components with N_k less than ``vb_prune``
 
 
 # ----------------------------------------
