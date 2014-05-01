74f5220dde137a594ed6eb27107d0f9949c71e79
Stephan <gu92biv@mytum.de>
2014-05-01 19:38:14 +0200

    [algorithms] add black box algorithm to obtain ``first_proposal``
    o add "first_proposal.py" with main algorithm
    o modify "mcmc.py" for more optional fine tuning
    o add "long_patches.py" --> formation of long patches summarized
                            --> minimal lex. integer partition into a function
    o include ``make_first_proposal`` into "make_interactive_env.py"

--- algorithms/mcmc.py
+++ algorithms/mcmc.py
@@ -2,11 +2,14 @@ from __future__ import division
 import numpy as np
 import pypmc
 
-def create_mcmc_samples(log_target, k, N_burn_in, N_MCMC, N_adapt, scale=.1):
+def create_mcmc_samples(log_target, k=10, N_burn_in=10**3, N_MCMC=2*10**4, N_adapt=500, scale=.1, upper=3., lower=-3.):
     """Run ``k`` markov-chains for ``N_burn_in`` steps, discard
     and run again for ``N_MCMC`` steps whereby the proposals are
     to be adapted every ``N_adapt`` steps.
-    Proposal covariances are initialized as np.eye(dim)*scale
+    Proposal covariances are initialized as np.eye(dim)*scale.
+    Starting values for the chains are randomly chosen within the
+    hypercube between ``upper`` and ``lower``.
+    Dimension is read from ``log_target.dim``.
 
     """
     dim = log_target.dim
@@ -18,9 +21,9 @@ def create_mcmc_samples(log_target, k, N_burn_in, N_MCMC, N_adapt, scale=.1):
     # choose random initializations to run k chains
     starts = []
     for i in range(k):
-        point = np.random.rand(dim)*6. - 3.
+        point = np.random.rand(dim)*(upper-lower) + lower
         while np.isinf(log_target(point)):
-            point = np.random.rand(dim)*6. - 3.
+            point = np.random.rand(dim)*(upper-lower) + lower
         starts.append(point)
 
     # define the markov chain objects
