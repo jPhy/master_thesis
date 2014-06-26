498171cf35cc88fa0b56973488196b765a6cba91
Stephan Jahn <gu92biv@mytum.de>
2014-06-26 19:51:29 +0200

    [Thesis/figures] add contour plot of Kilbinger banana

---
+++ Thesis/figures/Kilbinger_banana/plot_banana.py
@@ -0,0 +1,36 @@
+#! /usr/bin/python
+from math import exp
+import numpy as np
+import pypmc
+import matplotlib.pyplot as plt
+from banana_function import LogTarget
+
+# need only two dimension to plot the essential part of the Kilbinger banana
+log_target = LogTarget(2)
+target = lambda x: exp(log_target(x))
+
+# define the grid to plot the banana on
+N_x_points = 2000
+N_y_points = 2000
+x_begin = -25.
+x_end = 25.
+y_begin = -11.
+y_end = 6.
+
+x_delta = (x_end - x_begin) / N_x_points
+y_delta = (y_end - y_begin) / N_y_points
+grid = np.mgrid[x_begin : x_end:x_delta, y_begin:y_end:y_delta]
+
+# evaluate the target all over the grid
+values = np.empty(list(grid.shape)[1:])
+assert  values.shape == (grid.shape[1], grid.shape[2])
+for i in range(values.shape[0]):
+    for j in range(values.shape[1]):
+        values[i,j] = target(grid[:,i,j])
+
+# plot
+plt.figure()
+plt.contourf(grid[0], grid[1], values, cmap='gray_r')
+plt.xlabel('$x_1$')
+plt.ylabel('$x_2$')
+plt.savefig('banana.svg')
